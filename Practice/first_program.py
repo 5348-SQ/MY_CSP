a8c6a3afd3bf565d6d1c0e894c22d897e613729d
5348-SQ <miloplayingbaseball@gmail.com>
2026-01-22 13:27:30 -0700

    MY first_program_Python

---
+++ Practice/first_program.py
@@ -0,0 +1,6 @@
+#MY First_Program_Python
+
+
+Hotness = input("on a scale from 1 to 10, how hot are you?: ")
+
+print("thank you for thy input from now on until the end of time, on our records, you will be noted to have a hotness score of: ", Hotness)
---
+++ Practice/varibles_practice.py
@@ -0,0 +1,8 @@
+#MY Varibles practice
+# one can make comments in their code by puting a hash in and typeing stuff like this.
+
+no_name = "N/A"
+no_man2 = "2"
+no_name3 = "3"
+
+printe(no_name, no_nam2, no_name3)
