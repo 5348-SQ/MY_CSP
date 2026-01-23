f43877530adb1ebb9c1264fe8a7b8b09dfb92970
5348-SQ <miloplayingbaseball@gmail.com>
2026-01-23 16:32:44 -0700

    updated notes

--- Notes/Athimatic operations.py
+++ Notes/Athimatic operations.py
@@ -1,5 +1,5 @@
 #computer knows order of operations
-#order of operations:   1.()  2. exponents 2.5: int divistion (gives only whole number) 2.55 modulus (gives)  3.mult   4.div  5.add 6.sub
+#order of operations:   1.()  2. exponents 2.5: int divistion (gives only whole number) 2.55 modulus (division but only remainder of division)  3.mult   4.div  5.add 6.sub
 # (- = subtraction)  (+ = addition) (/ = divide) (mult = *) (exponent = **) (int div = //) (modulo %)
 
 apples = int(input("how many apples are there: "))
