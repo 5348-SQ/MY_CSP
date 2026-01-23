f4079865e694527bbc253f08bc7a9538baffaa64
5348-SQ <miloplayingbaseball@gmail.com>
2026-01-23 01:03:25 +0000

    Finished_varibles_practice

--- Practice/varibles_practice.py
+++ Practice/varibles_practice.py
@@ -1,8 +1,16 @@
 #MY Varibles practice
 # one can make comments in their code by puting a hash in and typeing stuff like this.
 
-no_name = "N/A"
-no_man2 = "2"
-no_name3 = "3"
+name = "N/A"
+no_nan2 = "2"
+no_name3 = "101"
+breakfast = "monosacaride"
+favcolor = "blue"
+schoolname = "UCAS"
+year = 2026
+eyecolor = "I_will_check_later"
+myage = 5348
+favsubject = "ALL OF THEM"
 
-print(no_name, no_nam2, no_name3)
+
+print(name, no_nan2, no_name3, breakfast, favcolor, schoolname, year, eyecolor, myage, favsubject)
