3254b1362d72974af9e9073eca10991a8316ca7c
5348-SQ <miloplayingbaseball@gmail.com>
2026-01-28 13:37:31 -0700

    made a silly thing

---
+++ Practice/silly_sentences.py
@@ -0,0 +1,17 @@
+#MY silly_sentences
+
+noun_A = input("Entity operating this entity, give the machine spirit a verb, that ends with -ing: ").strip().lower()
+
+verbed_A = input("Entity operating this entity, give the machine spirit a verb, that ends with -ed: ").strip().lower()
+
+verb_B = input("Entity operating this entity, give the machine spirit a verb: ").strip().lower()
+
+noun_B = input("Entity operating this entity, give the machine spirit a noun: ").strip().lower()
+
+adj_A = input("Entity operating this entity, give the machine spirit what the mortals call an adjative: ").strip().lower()
+
+pnoun_A = input("Entity operating this entity, give the machine spirit a littlery proper noun: ").strip().upper()
+
+print("Entity, thy seed has be blessed with this passage of scripture")
+
+print("At the end of your", noun_A, "you", verbed_A + ",", "causing", pnoun_A, "to", verb_B, "all", noun_B + "s" + ". In the End, it was worth the effort of", noun_A + ", due to it being", adj_A + ".")
