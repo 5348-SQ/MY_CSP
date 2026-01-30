8f53ebc4824327baeca730fa130bab0c0ffc069b
5348-SQ <miloplayingbaseball@gmail.com>
2026-01-30 13:43:54 -0700

    Made time of day

---
+++ Practice/Time of Day.py
@@ -0,0 +1,34 @@
+#MY Time of Day
+
+time = int(input("What is the hour in military time?: "))
+
+if time < 0:
+    print("An instence of entity DECO-UI has been loged, thank you for the report Agent.")
+elif time == 0:
+    print("A new Horizen has oppened ")
+elif time == 42:
+    print("It makes me glad that you understand it too")
+elif time == 67:
+    print("........You Brat")
+elif time <= 3:
+    print("Who do you think you are? A vampire? Stop answeing the call of these nights, its not healthy")
+    print("unless you are a late night wotker, if so, RIDE ON!")
+elif time == 4:
+    print("up again I see. It is unfortunate to see you here")
+elif time == 5:
+    print("Your an Early Bird, ey?")
+elif time == 6:
+    print("Oh shoot, I gota wake up for shcool")
+elif time == 7:
+    print("MEELECHK! IT IS THE WORST NUMBER. MAY ITS MUTIPLES BE CURSED!")
+elif time == 8:
+    print("I geuss your chinese or somthing")2
+elif time >= 9 and time <= 16:
+    print("WORK WORK WORK")
+    print("That was an allusion by the way")
+elif time > 16 and time < 21:
+    print("REST REST REST")
+elif time > 21 and time <= 24:
+    print("You need to be asleep, You need to be asleep, You need to be asleep, You need to be asleep, You need to be asleep,You need to be asleep, You need to be asleep, You need to be asleep, You need to be asleep, You need to be asleep.")
+else:
+    print("An instence of entity DECO-UI has been loged, thank you for the report Agent.")
