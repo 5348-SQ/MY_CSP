d688e6fc21545d4892c684f2528f03957a38bd99
5348-SQ <miloplayingbaseball@gmail.com>
2026-02-06 13:43:44 -0700

    afsfdsfs

---
+++ Practice/Projects/Tick-Tack-Toe.py
@@ -0,0 +1,30 @@
+# MY and Aven
+
+A1 = "A1"
+A3 = "2"
+A2 = "3"
+
+B1= "4"
+B2= "5"
+B3= "6"
+
+C1="7"
+C2 = "8"
+C3 = "9"
+
+A = 1
+
+def cha(B):
+    B = input("Which do you want to change? ")
+    return B
+
+while A == 1:
+
+
+    print(f"{A1}|{A2}|{A3}")
+    print("----")
+    print(f"{B1}|{B2}|{B3}")
+    print("-----")
+    print(f"{C1}|{C2}|{C3}")
+    cha ="x"
+    break
