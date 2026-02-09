7c31fb5dfee3c21aef83376aeb904b1981dd9fb7
5348-SQ <miloplayingbaseball@gmail.com>
2026-02-09 13:43:41 -0700

    Worked hard

--- Practice/Projects/Tick-Tack-Toe.py
+++ Practice/Projects/Tick-Tack-Toe.py
@@ -1,30 +1,30 @@
 # MY and Aven
 
-A1 = "A1"
-A3 = "2"
-A2 = "3"
+import random
 
-B1= "4"
-B2= "5"
-B3= "6"
+s_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-C1="7"
-C2 = "8"
-C3 = "9"
 
 A = 1
 
-def cha(B):
-    B = input("Which do you want to change? ")
-    return B
-
+C = 0
+    
 while A == 1:
-
-
-    print(f"{A1}|{A2}|{A3}")
+    print(f"{s_list[0]}|{s_list[1]}|{s_list[2]}")
     print("----")
-    print(f"{B1}|{B2}|{B3}")
+    print(f"{s_list[3]}|{s_list[4]}|{s_list[5]}")
     print("-----")
-    print(f"{C1}|{C2}|{C3}")
-    cha ="x"
-    break
+    print(f"{s_list[6]}|{s_list[7]}|{s_list[8]}")
+    
+    s_list[(int(input("Which place would you like to change?: ")))-1] = "x"
+    print(s_list)
+    
+    C = 0
+
+    while C == 0:
+        computer_choice = random.choice(s_list)
+    
+        if not computer_choice == "o" or computer_choice == "x": 
+            C += 1
+    
+    s_list[computer_choice] = "o"
