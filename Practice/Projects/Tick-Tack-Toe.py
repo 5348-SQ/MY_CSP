f2da1882c2495fdae81e39903f23ba39b579036e
5348-SQ <miloplayingbaseball@gmail.com>
2026-02-10 13:43:40 -0700

    Made things
    Co-authored-by: Mochi-Waffles <Mochi-Waffles@users.noreply.github.com>

--- Practice/Projects/Tick-Tack-Toe.py
+++ Practice/Projects/Tick-Tack-Toe.py
@@ -2,29 +2,99 @@
 
 import random
 
-s_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+def who_win(X, Y, Z):
+    if X == "x" and Y == "x" and Z == "x":
+        print("Player wins")
+        return 1
+    elif X == "o" and Y == "o" and Z == "o":
+        print("Computer wins")
+        return 2
+    else:
+        return 0
+        
 
+
+
+
+
+s_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 A = 1
 
 C = 0
     
 while A == 1:
+    
     print(f"{s_list[0]}|{s_list[1]}|{s_list[2]}")
     print("----")
     print(f"{s_list[3]}|{s_list[4]}|{s_list[5]}")
     print("-----")
     print(f"{s_list[6]}|{s_list[7]}|{s_list[8]}")
     
-    s_list[(int(input("Which place would you like to change?: ")))-1] = "x"
-    print(s_list)
+#player answer zone
+
+    ina = int(input("Which place would you like to change?: "))
+
+    D = 0
+
+    while D == 0:
     
+        if not ina == "o" or ina == "x": 
+            D += 1
+
+
+    s_list[ina-1] = "x"
+
+#computer answer zone
+
     C = 0
 
     while C == 0:
+        #random.choice returns the item itself not the index # of the item
         computer_choice = random.choice(s_list)
-    
-        if not computer_choice == "o" or computer_choice == "x": 
+        print(computer_choice)
+        if not computer_choice == "o" and not computer_choice == "x": 
             C += 1
     
-    s_list[computer_choice] = "o"
+    s_list[computer_choice-1] = "o"
+
+    A += who_win( s_list[0], s_list[1], s_list[2])
+
+    A += who_win( s_list[3], s_list[4], s_list[5])
+
+    A += who_win( s_list[6], s_list[7], s_list[8])
+
+    A += who_win( s_list[0], s_list[4], s_list[8])
+
+    A += who_win( s_list[2], s_list[7], s_list[8])
+
+    A += who_win( s_list[0], s_list[3], s_list[6])
+
+    A += who_win( s_list[1], s_list[4], s_list[7])
+
+    A += who_win( s_list[2], s_list[5], s_list[8])    
+
+
+if A == 1:
+    #then player won
+    print("w")
+    print(A)
+
+    print(f"{s_list[0]}|{s_list[1]}|{s_list[2]}")
+    print("----")
+    print(f"{s_list[3]}|{s_list[4]}|{s_list[5]}")
+    print("-----")
+    print(f"{s_list[6]}|{s_list[7]}|{s_list[8]}")
+
+
+    
+elif A == 2:
+    #then com won
+    print("L")
+
+    print(A)
+    print(f"{s_list[0]}|{s_list[1]}|{s_list[2]}")
+    print("----")
+    print(f"{s_list[3]}|{s_list[4]}|{s_list[5]}")
+    print("-----")
+    print(f"{s_list[6]}|{s_list[7]}|{s_list[8]}")
