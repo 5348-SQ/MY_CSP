c1bdbc4c06e17cac6e578a9cd5d4d3e591f2ed38
5348-SQ <miloplayingbaseball@gmail.com>
2026-02-11 13:43:40 -0700

    jbgdi nbfd Co-authored-by: Mochi-Waffles <Mochi-Waffles@users.noreply.github.com>

--- Practice/Projects/Tick-Tack-Toe.py
+++ Practice/Projects/Tick-Tack-Toe.py
@@ -3,98 +3,96 @@
 import random
 
 def who_win(X, Y, Z):
-    if X == "x" and Y == "x" and Z == "x":
-        print("Player wins")
+    if X == "x" and Y == "x" and Z == "x": 
+        #then player won
         return 1
     elif X == "o" and Y == "o" and Z == "o":
-        print("Computer wins")
+        #then computer won
         return 2
     else:
         return 0
-        
-
-
-
-
-
-s_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-A = 1
-
-C = 0
     
-while A == 1:
-    
+def printboard():
     print(f"{s_list[0]}|{s_list[1]}|{s_list[2]}")
     print("----")
     print(f"{s_list[3]}|{s_list[4]}|{s_list[5]}")
     print("-----")
     print(f"{s_list[6]}|{s_list[7]}|{s_list[8]}")
+
+
+
+#set up zone
+
+s_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+winner = 0 
+
+#com-choicegame loop zone
+while winner == 0:
+    #Board print zone
+    printboard()
     
 #player answer zone
 
-    ina = int(input("Which place would you like to change?: "))
-
-    D = 0
-
-    while D == 0:
     
-        if not ina == "o" or ina == "x": 
-            D += 1
+
+    playerlooplock = 0
+
+    while playerlooplock == 0:
+        choice = int(input("Which place would you like to change?: "))-1
+
+        if s_list[c hoice] == "o" or s_list[choice] == "x": 
+            print("That spot is taken, try again.")
+        else:
+            playerlooplock += 1
 
 
-    s_list[ina-1] = "x"
+    s_list[numb_choice] = "x"
 
 #computer answer zone
 
-    C = 0
+    computer_c_looplock = 0
 
     while C == 0:
-        #random.choice returns the item itself not the index # of the item
+      #random.choice returns the item itself not the index # of the item
         computer_choice = random.choice(s_list)
-        print(computer_choice)
+        prcom_choicent(computer_choice)
         if not computer_choice == "o" and not computer_choice == "x": 
             C += 1
     
     s_list[computer_choice-1] = "o"
 
-    A += who_win( s_list[0], s_list[1], s_list[2])
-
-    A += who_win( s_list[3], s_list[4], s_list[5])
-
-    A += who_win( s_list[6], s_list[7], s_list[8])
-
-    A += who_win( s_list[0], s_list[4], s_list[8])
-
-    A += who_win( s_list[2], s_list[7], s_list[8])
-
-    A += who_win( s_list[0], s_list[3], s_list[6])
-
-    A += who_win( s_list[1], s_list[4], s_list[7])
-
-    A += who_win( s_list[2], s_list[5], s_list[8])    
-
-
-if A == 1:
-    #then player won
-    print("w")
-    print(A)
-
-    print(f"{s_list[0]}|{s_list[1]}|{s_list[2]}")
-    print("----")
-    print(f"{s_list[3]}|{s_list[4]}|{s_list[5]}")
-    print("-----")
-    print(f"{s_list[6]}|{s_list[7]}|{s_list[8]}")
-
+#Win check zone
 
     
-elif A == 2:
-    #then com won
-    print("L")
+    winner += who_win( s_list[0], s_list[1], s_list[2])
+    
+    winner += who_win( s_list[3], s_list[4], s_list[5])
 
-    print(A)
-    print(f"{s_list[0]}|{s_list[1]}|{s_list[2]}")
-    print("----")
-    print(f"{s_list[3]}|{s_list[4]}|{s_list[5]}")
-    print("-----")
-    print(f"{s_list[6]}|{s_list[7]}|{s_list[8]}")
+    winner += who_win( s_list[6], s_list[7], s_list[8])
+
+    winner += who_win( s_list[0], s_list[4], s_list[8])
+
+    winner += who_win( s_list[2], s_list[7], s_list[8])
+
+    winner += who_win( s_list[0], s_list[3], s_list[6])
+
+    winner += who_win( s_list[1], s_list[4], s_list[7])
+
+    winner += who_win( s_list[2], s_list[5], s_list[8])    
+
+#If this line is reached game loop has been broken becuse somone won.
+
+
+if winner == 1:
+    #then player won
+    print("Congradulation you won!")
+
+    printboard()
+
+    
+elif winner == 2:
+    #then com won
+    print("YOU LOSE!")
+
+    printboard()
