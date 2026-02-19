09a107d0ec6db123822f4a18681ddeafce41b122
5348-SQ <miloplayingbaseball@gmail.com>
2026-02-19 15:43:12 -0700

    Yes

---
+++ C/notes/practice/varibles_practice.c
@@ -0,0 +1,35 @@
+//MY 6 Variables Practice
+
+#include <stdio.h>
+int main(){
+    char name[50] = "hiihihihihihihihihihihih";
+
+    int number = 1;
+
+    float gpa = 3.99999;
+
+    char first_meal[50] = "food";
+
+    char color[50] = "blue";
+
+    char schoolname[50] = "yes_UCAS";
+
+    int year = 1984;
+
+    char other_color[50] = "my eye color";
+
+    char age[50] = "Enough";
+
+    char fav_sub[50] = "ALL OF THEM";
+
+    printf("name;%s \n", name);
+    printf("number;%d\n", number);
+    printf("GPA;%f\n", gpa);
+    printf("meal;%s\n", first_meal);
+    printf("color;%s\n", color);
+    printf("school;%s\n", schoolname);
+    printf("year;%d\n", year);
+    printf("fav_color;%s\n", other_color);
+    printf("age;%s\n", age);
+    printf("favsubject;%s\n", fav_sub);
+}
