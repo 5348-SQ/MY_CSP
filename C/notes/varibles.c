fd818daf2fb3fccf0d8683335127217eb9fab554
5348-SQ <miloplayingbaseball@gmail.com>
2026-02-19 13:15:10 -0700

    finished notes

--- C/notes/varibles.c
+++ C/notes/varibles.c
@@ -7,14 +7,25 @@
 
 int main(){
     int number = 8;
-    float pi = 3.14;
+    const float pi = 3.14;
     char grade = 'A'; //single charicters uses single quotes
     char name[] = "Jake"; //strings use [] after naming the variable
+    char user[50];
+    //to creat an input in C. 1, creat a variable. 2, ask the user a question. 3, collect the answer (use scanf for this)
+
+    printf("GIVE NAME: \n");
+    scanf("%s", &user);
+
+    printf("GIVE NUMBER: \n");
+
+    scanf("%d", &number);
+    // if input is not int it becomes 0
+
 
     //use "/n" to break a line, you can do mutiple \n to skip mutiple lines
 
     printf("%d\n", number);
     printf("the digits of pi are: %f\n", pi);
-    printf("Your name is %s you are %d old. You have an %c in programing. pi is %f\n", name, number, grade, pi);
+    printf("Your name is %s you are %d old. You have an %c in programing. pi is %f\n", user, number, grade, pi);
     return 0;
 }
