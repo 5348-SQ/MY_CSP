3523f89d08a15b96d7a0e49d465200919a09cb04
5348-SQ <miloplayingbaseball@gmail.com>
2026-02-20 23:01:32 +0000

    Made firsy

---
+++ C/notes/practice/first_program.c
@@ -0,0 +1,13 @@
+//MY 6 first program
+
+#include <stdio.h>
+
+int main(){
+    char user[50] = "";
+    printf("GIVE NAME: \n");
+    scanf("%s", &user);
+    
+    pritf("Hello %s, thank you for fixing VS code for me", &user);
+
+    return 0;
+}
