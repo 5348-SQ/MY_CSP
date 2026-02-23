84228c528c720a2ecaaea680b94cff283baba0da
5348-SQ <miloplayingbaseball@gmail.com>
2026-02-23 13:39:01 -0700

    sad

--- C/notes/practice/Financial_calculator.c
+++ C/notes/practice/Financial_calculator.c
@@ -1,14 +1,28 @@
 //MY P6 Financial_Calculator
 
 int main(){
+    float inco;
     float rent;
-    float income;
     float util;
-    float groc
+    float groc;
+    float tran;
     
     
-    printf("What is thy rent?: ");
-    scanf
+    printf("What is thy income?:$ ");
+    scanf("%f", &inco);
 
+    printf("\nWhat is thy rent?:$ ");
+    scanf("%f", &rent);
+
+    printf("\nWhat is thy utilities?:$ ");
+    scanf("%f", &util);
+
+    printf("\nWhat is thy groceries:$ ");
+    scanf("%f", &groc);
+
+    printf("\nWhat is thy transportation expence?:$ ");
+    scanf("%f", &inco);
+
+    printf("\nYour rent is %f and that is %.2f precent of your income", &rent, &(rent/inco)*100);
 
 }
