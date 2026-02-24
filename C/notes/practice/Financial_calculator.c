f7147b869bc4d716ce8bb500fc8aab49c4fc6510
5348-SQ <miloplayingbaseball@gmail.com>
2026-02-24 23:58:20 +0000

    ss

--- C/notes/practice/Financial_calculator.c
+++ C/notes/practice/Financial_calculator.c
@@ -28,8 +28,18 @@ int main(){
     printf("\nWhat is thy transportation expence?:$ ");
     scanf("%f", &tran);
 
-    printf("\nYour rent is %.2f and that is %.2f precent of your income", rent, (rent/inco)*100);
+    printf("\nYour rent is $%.2f and that is %.2f precent of your income", rent, (rent/inco)*100);
 
+    printf("\nYour utilities is $%.2f and that is %.2f precent of your income", util, (util/inco)*100);
+
+    printf("\nYour groceries is $%.2f and that is %.2f precent of your income", groc, (groc/inco)*100);
+
+    printf("\nYour transportation expences sum to $%.2f, which is %.2f precent of your income", groc, (groc/inco)*100);
+
+    printf("It is advised you add $%.2f to your savings", inco * 0.1);
+
+    prinft("This leaves you with $%.2f to spend", inco-((groc+util+rent+tran)+ inco * 0.1 ));
+    //PLEASE TEACH US HOW TO WRITE FUNCTIONS ALREADY
     return 0;
 
 }
