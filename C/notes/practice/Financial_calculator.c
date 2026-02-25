d47be8bf72b9b13b1edf010c149cf9a8cb8c7964
5348-SQ <miloplayingbaseball@gmail.com>
2026-02-25 00:05:08 +0000

    FINISHED!!!

--- C/notes/practice/Financial_calculator.c
+++ C/notes/practice/Financial_calculator.c
@@ -13,32 +13,32 @@ int main(){
     float tran;
     
     
-    printf("What is thy income?:$ ");
+    printf("What is thy income?: $");
     scanf("%f", &inco);
 
-    printf("\nWhat is thy rent?: ");
+    printf("\nWhat is thy rent?: $");
     scanf("%f", &rent);
 
-    printf("\nWhat is thy utilities?:$ ");
+    printf("\nWhat is thy utilities?: $");
     scanf("%f", &util);
 
-    printf("\nWhat is thy groceries:$ ");
+    printf("\nWhat is thy groceries: $");
     scanf("%f", &groc);
 
-    printf("\nWhat is thy transportation expence?:$ ");
+    printf("\nWhat is thy transportation expence?: $");
     scanf("%f", &tran);
 
-    printf("\nYour rent is $%.2f and that is %.2f precent of your income", rent, (rent/inco)*100);
+    printf("\nYour rent is $%.2f and that is %.2f precent of your income.", rent, (rent/inco)*100);
 
-    printf("\nYour utilities is $%.2f and that is %.2f precent of your income", util, (util/inco)*100);
+    printf("\nYour utilities is $%.2f and that is %.2f precent of your income.", util, (util/inco)*100);
 
-    printf("\nYour groceries is $%.2f and that is %.2f precent of your income", groc, (groc/inco)*100);
+    printf("\nYour groceries is $%.2f and that is %.2f precent of your income.", groc, (groc/inco)*100);
 
-    printf("\nYour transportation expences sum to $%.2f, which is %.2f precent of your income", groc, (groc/inco)*100);
+    printf("\nYour transportation expences sum to $%.2f, which is %.2f precent of your income.", groc, (groc/inco)*100);
 
-    printf("It is advised you add $%.2f to your savings", inco * 0.1);
+    printf("\nIt is advised you add $%.2f to your savings.", inco * 0.1);
 
-    prinft("This leaves you with $%.2f to spend", inco-((groc+util+rent+tran)+ inco * 0.1 ));
+    printf("\nThis leaves you with $%.2f to spend.\n\n", inco-((groc+util+rent+tran)+ inco * 0.1 ));
     //PLEASE TEACH US HOW TO WRITE FUNCTIONS ALREADY
     return 0;
 
