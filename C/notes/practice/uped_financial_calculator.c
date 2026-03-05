e5ef256f309cfdf1fa630aa0c13232b107f21b91
5348-SQ <miloplayingbaseball@gmail.com>
2026-03-05 13:44:01 -0700

    as

---
+++ C/notes/practice/uped_financial_calculator.c
@@ -0,0 +1,21 @@
+//My P6 Updated Financial calculator
+
+#include <stdio.h>
+
+int precentfinder(part){
+
+    return round(part/income);
+}
+
+float num_getter(char is){
+    printf("MORTAL, WHAT IS THY %s?: $", is);
+    scanf("%f", num);
+    return num;
+}
+
+int main(){
+    income = 
+
+
+    return 0;
+}
